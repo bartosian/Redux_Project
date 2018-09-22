580c3edc77e2480e052b9affed9346f771fd4d62
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-22 18:57:35 -0400

    Redux in action

--- redux-basics.js
+++ redux-basics.js
@@ -1,7 +1,10 @@
 const redux = require('redux');
 const createStore = redux.createStore;
-
-const rootReducer = (state, action) {
+const initialState = {
+    counter: 0
+};
+const rootReducer = (state=initialState, action) => {
     return state;
 };
 const store = createStore(rootReducer);
+console.log(store.getState());
