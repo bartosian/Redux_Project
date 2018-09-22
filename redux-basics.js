ae982a11c67bc6c7536e6f9b26fac97508b4c110
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-22 19:08:19 -0400

    Added actions to store

--- redux-basics.js
+++ redux-basics.js
@@ -4,7 +4,24 @@ const initialState = {
     counter: 0
 };
 const rootReducer = (state=initialState, action) => {
-    return state;
+    if(action.type === "INCR_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if(action.type === "ADD_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
 };
 const store = createStore(rootReducer);
+console.log(store.getState());
+
+store.dispatch({ type: "INCR_COUNTER"});
+store.dispatch({ type: "ADD_COUNTER", value: 20});
+
 console.log(store.getState());
