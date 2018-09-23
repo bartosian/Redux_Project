033af9b3a0dd7efb584c48c19a86e1d831333ab4
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-23 07:20:51 -0400

    Added provider and stateMap

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './store/redux';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
